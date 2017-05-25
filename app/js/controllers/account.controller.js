de88610afe135ea63870b39c90829b31841147b1
Drew Hill <mdude2314@gmail.com>
2017-05-24 20:54:02 -0400

    session ownership work including adding and removing sessions from the account page

--- app/js/controllers/account.controller.js
+++ app/js/controllers/account.controller.js
@@ -4,11 +4,31 @@ app.controller('accountCtrl', [
     'Auth',
     '$state',
     '$scope',
+    '$location',
     '$firebaseObject',
     '$firebaseArray',
-    function (Auth, $state, $scope, $firebaseObject, $firebaseArray) {
+    function (Auth, $state, $scope, $location, $firebaseObject, $firebaseArray) {
+        const usersRef = firebase
+            .database()
+            .ref()
+            .child('users/');
+        const sessionsRef = firebase
+            .database()
+            .ref()
+            .child('sessions/');
+        const usersSessionsRef = firebase
+            .database()
+            .ref()
+            .child('users_sessions/');
+
         $scope.auth = null;
         $scope.user = null;
+        $scope.newSession = {
+            id: null,
+            title: null,
+            description: null
+        }
+        $scope.addingSession = false;
         Auth.$onAuthStateChanged(function (firebaseUser) {
             $scope.auth = firebaseUser;
             let usersRef = firebase
@@ -16,14 +36,46 @@ app.controller('accountCtrl', [
                 .ref()
                 .child("users/" + $scope.auth.uid);
             let userObj = $firebaseObject(usersRef);
-            userObj
-                .$bindTo($scope, "user")
-                .then(function () {
-                    console.log($scope.user);
-                });
+            userObj.$bindTo($scope, "user");
 
-            $scope.ownedSessions = $firebaseArray(usersRef.child('ownedSessions'))
-            $scope.ownedSessions.$loaded().then(res => {console.log(res)})
+            let currentUserSessionsRef = usersSessionsRef.child($scope.auth.uid);
+            $scope.ownedSessions = $firebaseArray(currentUserSessionsRef);
         });
+
+        this.goToSession = (sessionId) => {
+            $location.path('questions/' + sessionId);
+        }
+
+        this.removeSession = (session) => {
+            sessionsRef
+                .child(session.$id)
+                .remove();
+            usersSessionsRef
+                .child($scope.auth.uid + '/' + session.$id)
+                .remove();
+        }
+
+        this.addSession = () => {
+            sessionsRef
+                .child($scope.newSession.id)
+                .set({title: $scope.newSession.title, description: $scope.newSession.description, sessionOwnerUID: $scope.auth.uid});
+
+            usersSessionsRef
+                .child($scope.auth.uid + '/' + $scope.newSession.id)
+                .set({description: $scope.newSession.description, title: $scope.newSession.title});
+
+            $scope.newSession.title = null;
+            $scope.newSession.description = null;
+            $scope.newSession.id = null;
+            $scope.addingSession = false;
+        }
+
+        this.startAdd = () => {
+            $scope.addingSession = true;
+        }
+
+        this.cancelAdd = () => {
+            $scope.addingSession = false;
+        }
     }
 ]);
